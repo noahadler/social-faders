fbd284d81abe6d24ef62b06c1dd90b313cded2fb
Noah Adler <noah.adler@gmail.com>
2014-09-28 22:24:59 -0400

    Update package to 0.9

--- package.js
+++ package.js
@@ -1,10 +1,13 @@
 Package.describe({
-  summary: "Social Faders - multitouch, multiuser, realtime controls"
+  summary: "Social Faders - multitouch, multiuser, realtime controls",
+  version: "0.1.0",
+  git: "https://github.com/noahadler/social-faders.git"
 });
 
 Package.on_use(function(api) {
+  api.versionsFrom("METEOR@0.9.0");
   api.use('coffeescript', ['client','server']);
-  api.use('collection2', ['client','server']);
+  api.use("aldeed:collection2@0.2.16", ['client','server']);
   api.use('accounts-base', ['client', 'server']);
   //api.use('srp', ['client', 'server']);
   api.use(['underscore', 'templating', 'bootstrap', 'ui', 'spacebars'], 'client');
